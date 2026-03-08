b771faa6e299e75eb1bd6774b2cc933cb96bf518
kagame458 <hope@geoskills.ai>
2026-03-08 18:56:29 +0300

    Set up mood tracker page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mood_tracker/counter/counter.dart';
 import 'package:mood_tracker/l10n/l10n.dart';
+import 'package:mood_tracker/mood_tracker/_index.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -16,7 +16,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const MoodTrackerPage(),
     );
   }
 }
--- lib/mood_tracker/_index.dart
+++ lib/mood_tracker/_index.dart
@@ -1,3 +1,4 @@
 export 'cubit/_index.dart';
 export 'data/data.dart';
 export 'services/mood_tracker_service.dart';
+export 'view/mood_tracker_page.dart';
